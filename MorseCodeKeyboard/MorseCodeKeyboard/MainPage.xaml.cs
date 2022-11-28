7fc6b357aeb48b3bd2f81058ff9749417c297dd9
Andrew Gunn <andrewmgunn26@gmail.com>
2022-11-28 13:04:44 -0600

    init

---
+++ MorseCodeKeyboard/MorseCodeKeyboard/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCodeKeyboard
+{
+    public partial class MainPage : ContentPage
+    {
+        private string sentence { get; set; }
+        private string dotsAndDashes { get; set; }
+
+        public MainPage() {
+            // has to be here for each page
+            InitializeComponent();
+            sentence = "";
+            dotsAndDashes = "";
+        }
+
+        void ButtonClicked(object sender, EventArgs args) {
+            if (((Button)sender).Text == ".") {
+                if (((Button)sender).ClassId == "dot") {
+                    dotsAndDashes += ".";
+                    outputMorseText.Text = dotsAndDashes;
+                }
+            } else if (((Button)sender).Text == "-") {
+                if (((Button)sender).ClassId == "dash") {
+                    dotsAndDashes += "-";
+                    outputMorseText.Text += ".";
+                }
+            } else {
+                if (((Button)sender).ClassId == "space") { 
+                    outputText.Text += Morse.MorseCoder(dotsAndDashes);
+                    dotsAndDashes = "";
+                }
+            }
+            outputMorseText.Text = dotsAndDashes;
+        }
+    }
+}
